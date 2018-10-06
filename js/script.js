ec53f3cb163ad3d90e11ec9439251742adfd280f
mukovozartem <37810636+mukovozartem@users.noreply.github.com>
2018-10-06 14:10:44 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 function multiply1(){
     var num1, num2, num3, result1;
-    num1=document.getElementsByClassName("n1").value;
+    num1=document.getElementById("n1").value;
     num1=parseInt(num1); 
-    num2=document.getElementsByClassName("n2").value;
+    num2=document.getElementById("n2").value;
     num2=parseInt(num2);
-    num3=document.getElementsByClassName("n3").value;
+    num3=document.getElementById("n3").value;
     num3=parseInt(num3);
     result=(num1*num2*num3)/10000;
     document.getElementById("rd_js").innerHTML = result;
@@ -12,7 +12,7 @@ function multiply1(){
     
 function multiply2(){
     var num4,result2;
-    num4=document.getElementsByClassName("n4").value;
+    num4=document.getElementById("n4").value;
     num4=parseInt(num4);
     result=(num4*0.75);
     document.getElementById("lok_js").innerHTML = result;
