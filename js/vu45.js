a0b8640543de0d92659409da7edf8364db196919
mukovozartem <37810636+mukovozartem@users.noreply.github.com>
2018-11-26 12:20:28 +0200

    12213

--- js/vu45.js
+++ js/vu45.js
@@ -1,9 +1,12 @@
 function multiply1(){
-    var num1, result1;
+    var treb, fakt, num1, result1;
+    treb = 0.33;
+    fakt = 0.6;
+    
     num1=document.getElementsByClassName("n1").value;
     num1=parseInt(num1);
-    result1=(num1*0.33);
-    // result2=(num1*0.06);
+    result1=(num1*treb);
+    result2=(num1*fakt);
     document.getElementsByClassName("row_1_2_2").innerHTML = result1;
-    // document.getElementsByClassName("row_1_3_2").innerHTML = result2;
+    document.getElementsByClassName("row_1_3_2").innerHTML = result2;
 }
