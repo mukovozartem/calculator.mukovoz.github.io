f2c65b3ff2f2435c0e8cb11ec78aaa036a561dd6
Artem M <xenoniye@gmail.com>
2019-05-17 15:09:54 +0300

    23

--- js/fuel.js
+++ js/fuel.js
@@ -1,21 +1,21 @@
 function multiply1(){
     var num1, num2, num3, result1;
-    num1=document.getElementById("n1").value;
+    num1=document.getElementsByClassName("input-1").value;
     num1=parseInt(num1); 
-    num2=document.getElementById("n2").value;
+    num2=document.getElementsByClassName("input-2").value;
     num2=parseInt(num2);
-    num3=document.getElementById("n3").value;
+    num3=document.getElementsByClassName("input-3").value;
     num3=parseInt(num3);
     result=(num1*num2*num3)/10000;
-    document.getElementById("rd_js").innerHTML = result;
+    document.getElementsByClassName("output-1").innerHTML = result;
     }
     
 function multiply2(){
     var num4,result2;
-    num4=document.getElementById("n4").value;
+    num4=document.getElementsByClassName("input-4").value;
     num4=parseInt(num4);
     result=(num4*0.75);
-    document.getElementById("lok_js").innerHTML = result;
+    document.getElementsByClassName("output-2").innerHTML = result;
 }
 
 // Попробывать сделать ввод в поля только цифры
