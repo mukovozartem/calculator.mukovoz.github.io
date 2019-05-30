125bc364904655818dc1935bc83c2ac1b46a5171
Artem M <xenoniye@gmail.com>
2019-05-30 21:04:41 +0300

    30.05

--- js/fuel.js
+++ js/fuel.js
@@ -1,21 +1,21 @@
 function multiply1(){
     var num1, num2, num3, result1;
-    num1=document.getElementsByClassName("input-1").value;
-    num1=parseInt(num1); 
-    num2=document.getElementsByClassName("input-2").value;
-    num2=parseInt(num2);
-    num3=document.getElementsByClassName("input-3").value;
-    num3=parseInt(num3);
-    result1=(num1*num2*num3)/10000;
-    document.getElementsByClassName("output-1").innerHTML = result1;
+    num1 = document.getElementById("input-1").value;
+    num1 = parseInt(num1); 
+    num2 = document.getElementById("input-2").value;
+    num2 = parseInt(num2);
+    num3 = document.getElementById("input-3").value;
+    num3 = parseInt(num3);
+    result1 = (num1*num2*num3)/10000;
+    document.getElementById("output-1").innerHTML = result1;
     }
     
 function multiply2(){
     var num4,result2;
-    num4=document.getElementsByClassName("input-4").value;
-    num4=parseInt(num4);
-    result2=(num4*0.75);
-    document.getElementsByClassName("output-2").innerHTML = result2;
+    num4 = document.getElementById("input-4").value;
+    num4 = parseInt(num4);
+    result2 = (num4*0.75);
+    document.getElementById("output-2").innerHTML = result2;
 }
 
 // Попробывать сделать ввод в поля только цифры
